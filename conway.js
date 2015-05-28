5581981b6c395c151106f1f44e0714346e442d48
Scott Weiser <travstw@gmail.com>
2015-05-27 17:02:50 -0700

    Edge Cases, Fixed Array assign prob

--- conway.js
+++ conway.js
@@ -5,17 +5,20 @@ var next = [];
 var stringer;
 //var things = 0;
 reset();
+//console.log(current);  
 
 function randomArray(){
   for(var i = 0; i < 5000; i++){
-    current[i] = prob(i);   
-  }  
+    current[i] = prob(i);  
+
+  } 
+  
 }
 
 function prob(i){
-  var probTable= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+  var probTable= [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1];
 
-  var num = Math.floor((Math.random() * 50) -1);
+  var num = Math.floor((Math.random() * 20) );
   return probTable[num];
   // if(Math.round(Math.random()) === 1){
   //    return Math.round(Math.random());
@@ -100,10 +103,11 @@ function addToPage(){
   
 function nextGen(){
 	var friend;
-  prior = current;
+
+  
 	for(var x = 0; x < current.length; x++){
 		friend = friends(x);
-
+    
 		if (friend === 3){
 			next[x] = 1;
 		}
@@ -118,30 +122,138 @@ function nextGen(){
   printBoard(next);
   addToPage();
 
-  current = next;
+  //console.log(friend);
 
-}  
+
+  varTest();
+  
+  prior = current.slice(0);
+  varTest();
+
+  current = next.slice(0);
+  varTest();
+
+
+  
+
+} 
+
+var up;
+var upRight;
+var upLeft;
+var left;
+var right;
+var lowRight;
+var lowLeft;
+var down;
 
 function friends(x){
 	var totalNeighbors = 0;
-	var neighbors = [current[x], current[x-100], current[x-99], current[x+1], current[x+101], current[x+100], 
-	current[x+99], current[x-1], current[x-101]];
+  upCheck(x);
+  downCheck(x);
+  leftCheck(x);
+  rightCheck(x);
+ 
+	var neighbors = [current[x],up,upRight,right,lowRight,down,lowLeft,left,upLeft];
+  //console.log(neighbors);
 
 	for(var n = 0; n < neighbors.length; n++){
-		if(typeof neighbors[n] !== 'undefined'){
+		// if(typeof neighbors[n] !== 'undefined'){
 			totalNeighbors = totalNeighbors + neighbors[n]; 
-		}
+		//}
 	}
+  //console.log(x);
+  //console.log(neighbors);
 
 	return totalNeighbors;
 
 }
 
-// function priorGen(){
-//   stringer = "";
-//   printBoardPrior();
-//   addToPage();
-// }
+function upCheck(x){
+  if(typeof current[x - 100] === 'undefined'){
+    up = current[x+4900];
+    if(x === 0){
+      upLeft = current[x+4999];
+      upRight = current[x+4901];
+    }
+    else if (x === 99){
+      upLeft = current[x+4899];
+    upRight = current[x+4801];
+    }
+    else {
+      upLeft = current[x +4901];
+      upRight = current[x + 4899];
+    }
+  }
+  else {
+    up = current[x-100];
+    upLeft = current[x-101];
+    upRight = current[x-99];
+  }
+
+}
+
+function downCheck(x){
+  if(typeof current[x+100] === 'undefined'){
+    down = current[x - 4900];
+    lowLeft = current[x - 4901];
+    lowRight = current[x- 4899];
+  }
+  else {
+    down = current[x+100];
+    lowLeft = current[x+99];
+    lowRight = current[x+101];
+  }
+
+}
+
+function leftCheck(x){
+  if(typeof current[x-1] === 'undefined'){
+    left = current[x + 99];
+  }
+  else {
+    left = current[x-1];
+  }
+}
+
+function rightCheck(x){
+  for(var rCol = 99; rCol < 4999; rCol+=100 ){
+    if (x === rCol){
+      right = current[x - 99];
+      break;
+    }
+    else {
+      right = current[x+1];
+    }
+  }
+
+  
+
+}
+
+
+
+
+function priorGen(){
+  
+  stringer = "";
+  printBoard(prior);
+  addToPage();
+
+  
+
+  // console.log(current);
+  // console.log("**********************************************");
+  // console.log(next);
+}
+
+function currentGen(){
+  stringer = "";
+  printBoard(current);
+  addToPage();
+}
+
+
 
 
 
@@ -159,7 +271,21 @@ function stop(){
 }
 
 
+function varTest(){
+  if(current === prior){
+    console.log("prior = current");
 
-
+  }
+  if (prior === next){
+    console.log("prior = next");
+  }
+  if (current === next){
+    console.log("current = next");
+  }
+  console.log("******************");
+  // else {
+  //   console.log("nothing is equal");
+  // }
+}
 
 
